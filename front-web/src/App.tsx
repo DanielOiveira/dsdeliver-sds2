7d7a87d52b1efb60072930df968d76e1686794f8
DanielOiveira <danielcompositor1307@gmail.com>
2021-01-10 22:17:48 -0300

    Finish front-web

--- front-web/src/App.tsx
+++ front-web/src/App.tsx
@@ -1,11 +1,14 @@
 import './App.css';
 import Routes from './Routes';
+import { ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.min.css'; 
 
 function App() {
   return (
-   
+    <>
       <Routes />
-  
+      <ToastContainer />
+    </>
   )
 
   
